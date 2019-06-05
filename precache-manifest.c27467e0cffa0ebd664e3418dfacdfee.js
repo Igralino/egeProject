c633727a1766e88d211d86cd970bbe890f323a8e
Igralino <Igralino@ya.ru>
2019-06-05 22:49:09 +0300

    First

---
+++ precache-manifest.c27467e0cffa0ebd664e3418dfacdfee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d48730ead4fd500cb4631e74702e30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21144fc3336289f83720",
+    "url": "./static/css/2.b5769064.chunk.css"
+  },
+  {
+    "revision": "7028d44ee57d62d8456f",
+    "url": "./static/css/main.c9affafc.chunk.css"
+  },
+  {
+    "revision": "21144fc3336289f83720",
+    "url": "./static/js/2.f5cd5e8b.chunk.js"
+  },
+  {
+    "revision": "7028d44ee57d62d8456f",
+    "url": "./static/js/main.cd9e3cce.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
